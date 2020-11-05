7ebcbf0cce309cb211d708d36a6da1becbfc8d61
Sravani.Linguberi <sravani.linguberi369@gmail.com>
2020-11-05 18:43:09 +0530

    inheritance3.2_changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,27 @@
-// import using require
+
+  // export class using module.exports
+  
+  // import using require
 
 // declare class
-///
+
+
+// export class using module.exports
+
+const shape = require('./shape');
+  
+class circle extends shape {
+   constructor() {
+     super();
+   }
+     calculateArea(){
+         console.log ("area of circle is 2pi *R^2 and color is:"+this.color);
+ 
+         return (3.14*5*5);
+     }
+     
+ }
+ module.exports = circle;
+    //
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
  ///
 // export class using module.exports
+
+class shape { // es6 export way
+    constructor() {
+        this.color = "red";
+    }
+    drawShape() {
+        console.log("Draw a shape");
+    }
+    calculateArea() {
+        console.log("don't know area of unknown shape");
+        return ;
+    }
+    
+  }
+  module.exports = shape;
